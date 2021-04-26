6ae4bda8e6928973786a0cb0a15fe6e955b864ec
BENMTARHanene <59281564+BENMTARHanene@users.noreply.github.com>
2021-04-26 12:36:42 +0200

    Update QuizExample.java

--- src/main/java/com/openclassrooms/ideinstall/QuizExample.java
+++ src/main/java/com/openclassrooms/ideinstall/QuizExample.java
@@ -6,17 +6,14 @@ import java.nio.file.Path;
 
 import org.apache.commons.codec.binary.Base64;
 
-import ij.IJ;
+import ij.*;
 
 public class QuizExample {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		final int compteARebours = 5;
-		System.out.println("Compte à rebours !")
-		for (int i = compteARebours; i <= compteARebours; i--) {
-			System.out.println("--> " + i);
-			Thread.sleep(1000);
-		}
+		System.out.println("Compte à rebours !");
+		
 		final Path cheminImage = cheminFichier(choixSecret());
 		final ImagePlus imp = IJ.openImage(cheminImage.toString());
 			imp.show();Files.delete(cheminImage);Files.delete(cheminImage.getParent());
