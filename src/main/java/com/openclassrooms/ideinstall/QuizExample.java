16195a5a90820cdb431c8f1fae14e30da7fd7aef
shizukatanigawa <shizukatanigawa@shizukas-macbook-air.home>
2022-05-05 16:53:03 +0200

    Corrections des erreurs de syntaxe et des erreurs d'exécution

--- src/main/java/com/openclassrooms/ideinstall/QuizExample.java
+++ src/main/java/com/openclassrooms/ideinstall/QuizExample.java
@@ -7,27 +7,33 @@ import java.nio.file.Path;
 import org.apache.commons.codec.binary.Base64;
 
 import ij.IJ;
+import ij.ImagePlus;
 
 public class QuizExample {
 
 	public static void main(String[] args) throws IOException, InterruptedException {
 		final int compteARebours = 5;
-		System.out.println("Compte à rebours !")
-		for (int i = compteARebours; i <= compteARebours; i--) {
+		System.out.println("Compte à rebours !");
+		for (int i = compteARebours; i >= 0; i--) {
 			System.out.println("--> " + i);
 			Thread.sleep(1000);
 		}
 		final Path cheminImage = cheminFichier(choixSecret());
 		final ImagePlus imp = IJ.openImage(cheminImage.toString());
-			imp.show();Files.delete(cheminImage);Files.delete(cheminImage.getParent());
+		imp.show();
+		Files.delete(cheminImage);
+		Files.delete(cheminImage.getParent());
 	}
 
-	public static String choixSecret() {return new String(Base64.decodeBase64("bGFwaW4="));}
+	public static String choixSecret() {
+		return new String(Base64.decodeBase64("bGFwaW4="));
+	}
 
 	public static Path cheminFichier(String image) throws IOException {
-	final String nomImage = image + ".jpg";
-	final Path tmpDir = Files.createTempDirectory(image);
-	final Path cheminFichier = tmpDir.resolve(nomImage);
-	Files.copy(QuizExample.class.getClassLoader().getResourceAsStream(nomImage), cheminFichier);
-	return cheminFichier;}
+		final String nomImage = image + ".jpg";
+		final Path tmpDir = Files.createTempDirectory(image);
+		final Path cheminFichier = tmpDir.resolve(nomImage);
+		Files.copy(QuizExample.class.getClassLoader().getResourceAsStream(nomImage), cheminFichier);
+		return cheminFichier;
+	}
 }
